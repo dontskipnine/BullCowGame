d80e1694cdc908a090c4c820641213f30ba67a37
Zach <of7.ztaylor@gmail.com>
2018-11-25 16:33:03 -0600

    Initial Commit

---
+++ Section02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Section02
+//
+//  Created by Zachary Taylor on 11/25/18.
+//  Copyright © 2018 Zachary Taylor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
